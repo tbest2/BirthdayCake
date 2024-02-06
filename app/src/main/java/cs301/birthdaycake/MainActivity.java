a3ccb99a6831fd88197c42678b5b1c6438028189
DropT <best27@up.edu>
2024-02-06 11:08:14 -0800

    Day 1 of Lab 2

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -13,4 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
